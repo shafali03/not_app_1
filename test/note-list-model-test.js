1bcd4bf2fd3f48d88f4daa6b79ed9182f7c6cacc
shafali03 <shaf03sm@gmail.com>
2020-01-08 13:47:47 +0000

    test passing corrected html script tags

--- test/note-list-model-test.js
+++ test/note-list-model-test.js
@@ -6,14 +6,8 @@ function displayList() {
   list.add('water')
   assert.isTrue(list.displayNotes()[0] == 'coke')
   assert.isTrue(list.displayNotes()[1] == 'water')
+  
 }
 
 displayList();
 
-
-
-
-assert.isTrue(list.displayNotes()[0] == 'Academy');
-  assert.isTrue(list.displayNotes()[1] == 'Makers');
-}
-displayList();
