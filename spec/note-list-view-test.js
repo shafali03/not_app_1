3b3a44a74c7b893fad54b3df553deb019c6fc012
shafali03 <shaf03sm@gmail.com>
2020-01-11 21:13:29 +0000

    refactor note list view

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -2,11 +2,10 @@ function testNoteListView() {
   var noteList = new BookNote();
   var noteListView = new NoteListView(noteList)
 
-  noteList.add('food: pizza');
-  noteList.add('drink: water');
-
-  var result = noteListView.getHTML();
-  var expect = '<ul><li><div>food: pizza</div></li><li><div>drink: water</div></li></ul>'
-  assert.isTrue(result === expect)
+  noteList.add('coke');
+  noteList.add('water');
+  
+  assert.isTrue(noteListView.display() === '<ul><li><div>coke</div></li><li><div>water</div></li></ul>') 
+  console.log('NoteListView passed')
 };
 testNoteListView()
